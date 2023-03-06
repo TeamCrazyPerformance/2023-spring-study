1a4e8075ab6fc10d7af93a28927bdc63ab83bd7f
wlsh44 <k87913j@gmail.com>
2023-03-06 20:18:34 +0900

    feat: 메시지 추가

--- lotto/src/main/java/org/example/LottoApplication.java
+++ lotto/src/main/java/org/example/LottoApplication.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class LottoApplication {
     public static void main(String[] args) {
+        System.out.println("수정했습니다.");
         System.out.println("Hello world!");
     }
 }
