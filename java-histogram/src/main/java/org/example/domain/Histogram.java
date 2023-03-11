fdd1798ee4b9a090a45017f4cdb32b98f738e700
cshooon <0015chseang@gmail.com>
2023-03-11 13:25:08 +0900

    feat : 히스토그램 구현

--- java-histogram/src/main/java/org/example/domain/Histogram.java
+++ java-histogram/src/main/java/org/example/domain/Histogram.java
@@ -1,7 +1,20 @@
 package org.example.domain;
 
+import java.util.ArrayList;
+
 /**
  * 히스토그램 도메인 클래스
  */
 public class Histogram {
+    public ArrayList<String> drawing(ArrayList<Integer> numbers){
+        ArrayList<String> histogram = new ArrayList<>();
+        for(int num: numbers) {
+            String eq = "";
+            for (int i = 0; i < num; i++) {
+                eq += "=";
+            }
+            histogram.add(eq);
+        }
+        return histogram;
+    }
 }
--- java-histogram/src/main/java/org/example/view/OutputView.java
+++ java-histogram/src/main/java/org/example/view/OutputView.java
@@ -1,7 +1,18 @@
 package org.example.view;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
 /**
  * 결과 출력을 담당하는 클래스
  */
 public class OutputView {
+    public void print(ArrayList<String> histogram) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(String str: histogram)
+            bw.write(str + "\n");
+        bw.flush();
+    }
 }
