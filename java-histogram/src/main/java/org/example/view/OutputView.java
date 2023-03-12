2d6ab8eb8f81af5ab6f13aeaf1e1becba7750b8c
Hyeongjin Lee <lhjin0j@gmail.com>
2023-03-12 22:59:18 +0900

    refactoring

--- java-histogram/src/main/java/org/example/view/OutputView.java
+++ java-histogram/src/main/java/org/example/view/OutputView.java
@@ -8,13 +8,8 @@ import org.example.domain.Histogram;
  * 결과 출력을 담당하는 클래스
  */
 public class OutputView {
-  public void printHist(List<Histogram> hist) {
-    System.out.println("히스토그램 결입니다.");
-    for (Histogram e : hist) {
-      for(int i=0; i<e.getData(); i++) {
-        System.out.print("=");
-      }
-      System.out.println(); 
-    }
+  public void printHist(Histogram histogram) {
+    System.out.println("히스토그램 결과입니다.");
+    histogram.drawHistogram();
   }
 }
